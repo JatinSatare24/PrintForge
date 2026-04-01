88bc76669c4764e75c5a58119fb05fe568c30917
Jatin Satare <jatinsatare@gmail.com>
2026-04-01 20:42:57 +0530

    update tailwind css config for hover and active color change

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "orange-accent": "#F77D36"
+      }
+    }
   },
   plugins: [],
 }
