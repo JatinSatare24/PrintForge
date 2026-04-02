feaa220cc33f652ae6133e25905195864f6b3a0b
Jatin Satare <jatinsatare@gmail.com>
2026-04-02 23:39:48 +0530

    add filter model functionality to 3d-models page

--- app/3d-models/page.tsx
+++ app/3d-models/page.tsx
@@ -1,7 +1,37 @@
+import Form from "next/form"
+
 import ModelsGrid from "@/app/components/ModelsGrid"
+import type { ModelsPageProps } from "@/app/types"
 import { getModels } from "@/app/lib/models"
 
-export default async function Page() {
+/**
+ * Challenge: Upgrade our form to a Form from Next.js
+ */
+
+export default async function Page({ searchParams }: ModelsPageProps) {
     const models = await getModels()
-    return <ModelsGrid title="3D Models" models={models} />
+    const query = (await searchParams)
+
+    const filteredModels = query
+        ? models.filter(model =>
+            model.name.toLowerCase().includes(query.toLowerCase()) ||
+            model.description.toLowerCase().includes(query.toLowerCase())
+        )
+        : models
+
+    return (
+        <>
+            <Form action="/3d-models" className="w-full px-5 md:px-0 md:max-w-xl">
+                <input
+                    type="text"
+                    name="query"
+                    placeholder="E.g. dragon"
+                    autoComplete="off"
+                    defaultValue={query}
+                    className="w-full py-3 pl-5 pr-5 text-sm placeholder-gray-500 bg-white border border-[#606060] rounded-full focus:border-[#606060] focus:outline-none focus:ring-0 md:text-base"
+                />
+            </Form>
+            <ModelsGrid title="3D Models" models={filteredModels} />
+        </>
+    )
 }
